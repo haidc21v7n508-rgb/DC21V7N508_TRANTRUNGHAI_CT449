e9c98439386170ee3333d24e7ff5bc4166d0a640
Tran Trung Hai <haidc21v7n508@vlvh.ctu.edu.vn>
2025-09-18 01:19:47 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactRoutes = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json ({ message: "Welcome to contact book applicaiton" });
+
 });
+
+app.use("/api/contacts", contactRoutes);
+
 
 module.exports = app;
